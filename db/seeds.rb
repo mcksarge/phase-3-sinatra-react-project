3a46a873685ee32bb7b7f5a4d24df58190b1c8ea
Fred <higgins.fred.s@gmail.com>
2022-05-10 11:57:07 -0700

    Add seed data for Priorities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+    Priority.create([{name: "Emergency", timeframe: "1 hour"}{name: "High", timeframe: "3 hours"}, {name: "Medium", timeframe: "6 hours"}, {name: "Low", timeframe: "24 hours"}])
 
 puts "✅ Done seeding!"
