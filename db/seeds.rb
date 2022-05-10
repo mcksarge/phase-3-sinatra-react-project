cdd4fa887b5bb50a0c5bf9fb981ad2b0b1d520ad
Fred <higgins.fred.s@gmail.com>
2022-05-10 11:59:59 -0700

    Add seed data for Category and Task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "🌱 Seeding spices..."
 
     Priority.create([{name: "Emergency", timeframe: "1 hour"}{name: "High", timeframe: "3 hours"}, {name: "Medium", timeframe: "6 hours"}, {name: "Low", timeframe: "24 hours"}])
+    Category.create([{name: "Yard work"}, {name: "House work"}, {name: "Errands"}])
+    Task.create([{name: "Take out trash", priority: 1, category: 2}, {name: "Pull weeds", priority: 3, category: 1}, {name: "Buy groceries", priority: 4, category: 3}])
 
 puts "✅ Done seeding!"
